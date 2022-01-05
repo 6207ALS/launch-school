73ab86e0af240a5f22ef9c9c07b4832474164588
Allen Lee <soc.acc.allen@gmail.com>
2022-01-05 13:03:06 -0500

    Variables Exercise 3 (Edit)

--- variables/exercise_3.js
+++ variables/exercise_3.js
@@ -5,5 +5,5 @@
 console.log(foo);
 
 // The program will throw "Uncaught Reference Error: foo is not defined". As explained,
-// the foo variable within the brackets has block scope. It cannot be referenced outside
+// the foo variable within the curly braces has block scope. It cannot be referenced outside
 // of the block it is in. 
