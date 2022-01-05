638dce4e3662a96efa735034a7a4651bb781d46a
Allen Lee <soc.acc.allen@gmail.com>
2022-01-05 14:58:43 -0500

    Input/Output Exercise 2 (Edit)

--- input_output/exercise_2/greeter.js
+++ input_output/exercise_2/greeter.js
@@ -1,6 +1,6 @@
 
 let rlSync = require('readline-sync');
 let firstName = rlSync.question("What is your first name?\n");  // prompts user for first name
-let lastName = rlSync.question("What is your last name?\n");    // prompts user for last
+let lastName = rlSync.question("What is your last name?\n");    // prompts user for last name
 
 console.log(`Hello, ${firstName} ${lastName}!`);    // greets user's full name
