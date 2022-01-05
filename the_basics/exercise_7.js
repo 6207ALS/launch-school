c35844210359f952b625190ebe7880c1cd8ccc6d
Allen Lee <soc.acc.allen@gmail.com>
2022-01-05 11:47:29 -0500

    The Basics Exercise 7

---
+++ the_basics/exercise_7.js
@@ -0,0 +1,8 @@
+let foo = ['a', 'b', 'c'];
+console.log(foo.length);
+console.log(foo[3]);
+
+// The last line of code would throw an error because it is attempting
+// to access an index outside of the foo array's bounds. "foo" only
+// has three elements, whereas the expression attempts to access the 
+// fourth element.
