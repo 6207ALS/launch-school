05667c4aad85cfed833b1f93b85e57a6bd553bc7
Allen Lee <soc.acc.allen@gmail.com>
2022-01-07 19:00:41 -0500

    Arrays Exercise 4 (Edit)

--- arrays/exercise_4.js
+++ arrays/exercise_4.js
@@ -3,10 +3,10 @@ let myArray = [1, 3, 6, 11, 4, 2,
 
 // The evenOrOdd variable uses the .map() method on myArray to iterate over
 // each element. If the element is an even value, the corresponding element
-// in the new array is set to 'true'. If not, it is set as 'false'. 
+// in evenOrOdd's array is set to 'even'. If not, it is set as 'odd'. 
 
 let evenOrOdd = myArray.map(num => {
-  return (num % 2) === 0 ? 'true': 'false'
+  return (num % 2) === 0 ? 'even': 'odd'
 });
 
 console.log(evenOrOdd);
@@ -16,6 +16,6 @@ console.log(evenOrOdd);
 A struggle I had with using the arrow function was adding curly braces after 
 the arrow. Doing so would result in an array of undefined values. It turns out
 if I used curly braces my arrow function would require the 'return' keyword, 
-which I forgot to do. I would either have to add the return keyword to my 
-function or omit the curly braces. I chose the former option. 
+which I forgot to add. I would either have to add the return keyword to my 
+arrow function or omit the curly braces. I chose the former option. 
 */
