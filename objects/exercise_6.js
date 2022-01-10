5d429f3dedb29fb070bfca0424220bffed0859d1
Allen Lee <soc.acc.allen@gmail.com>
2022-01-09 21:43:20 -0500

    Objects Exercise 6

---
+++ objects/exercise_6.js
@@ -0,0 +1,17 @@
+/*
+1. "foo"
+2. 3.1415
+3. [ 'a', 'b', 'c' ]
+4. false
+5. foo
+6. function bar() { return "bar"; }
+7. undefined
+8. { a: 1, b: 2 }
+*/
+
+/*
+Of the values above, 1, 2, 4, and 7 are primitive values. They are a 
+string, number, boolean, and undefined, respectively. Values 3 and 8
+are an array and an object, respectively. The remaining (5 and 6) are
+neither primitive values nor objects. 
+*/
