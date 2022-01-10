bfad25c8b11fe71501d180970be45795f4d834cf
Allen Lee <soc.acc.allen@gmail.com>
2022-01-09 20:14:24 -0500

    Objects Exercise 1

---
+++ objects/exercise_1.js
@@ -0,0 +1,14 @@
+let person = {
+  name:       'Bob',
+  occupation: 'web developer',
+  hobbies:    'painting',
+};
+
+/*
+There are two ways to access the name of the person object: use bracket 
+notation with the key's name inside or use dot notation followed by the
+key's name.
+*/
+
+console.log(person['name']);
+console.log(person.name);
