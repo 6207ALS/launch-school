4c544be5495a4499ce45f2c0cc4e15861eda6112
Allen Lee <soc.acc.allen@gmail.com>
2022-01-10 19:34:33 -0500

    More Stuff Exercise 7

---
+++ more_stuff/exercise_7.js
@@ -0,0 +1,11 @@
+/*
+What is exception handling and what problem does it solve?
+
+Exception handling refers to dealing with program errors in a manageable,
+predictable way. It means preemptively dealing with errors before the program
+chooses to end. This means if a program happens to throw an error, and
+is coded to handle such error (notably through the try/catch statement), it
+will run an alternative body of code instead of ending the program. It 
+additionally allows programmers to pinpoint where a bug exists within
+a body of code. 
+*/
