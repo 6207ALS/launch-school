2607e9d316e84145c1947b8dc08ccc182fec2671
Allen Lee <6207ALS@gmail.com>
2022-03-21 17:27:34 -0400

    Easy 1: Odd Numbers

---
+++ easy-1/odd_numbers.js
@@ -0,0 +1,9 @@
+/*
+The following code logs all odd numbers from 1 to 99, inclusive, to the console.
+It uses a for loop to iterate over every number, and determines if the number
+is even or odd. 
+*/
+
+for (let i = 1; i <= 99; i++) {
+    if (i % 2 === 1) console.log(i);
+}
