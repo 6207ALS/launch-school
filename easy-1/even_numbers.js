87cd35edec3aae07dade3302e7313df92858315d
Allen Lee <6207ALS@gmail.com>
2022-03-21 20:46:24 -0400

    Easy 1: Even Numbers

---
+++ easy-1/even_numbers.js
@@ -0,0 +1,9 @@
+/*
+The following code logs all even numbers from 1 to 99, inclusive, to the 
+console. It simply uses a for loop that increments by values of 2 (starting from
+the value 2) to log every even number.  
+*/
+
+for (let i = 2; i < 99; i+=2) {
+    console.log(i);
+}
