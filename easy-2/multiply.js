56d11d6e563994692e3f2f0a82f72718d3c8f59e
Allen Lee <6207ALS@gmail.com>
2022-03-23 15:53:57 -0400

    Easy 2: Multiplying Two Numbers

---
+++ easy-2/multiply.js
@@ -0,0 +1,10 @@
+/*
+The multiply function simply returns the product of two provided numbers. 
+*/
+
+function multiply (num1, num2) {
+  return num1 * num2;
+}
+
+// test case
+console.log(multiply(5, 3) === 15); // logs true
