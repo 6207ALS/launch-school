1c7c4aa49e3a66a96cbee98010663d8e2f9bfb30
Allen Lee <6207ALS@gmail.com>
2022-03-25 20:49:45 -0400

    Easy 3: Stringy Strings (Edit)

--- easy-3/stringy.js
+++ easy-3/stringy.js
@@ -12,6 +12,7 @@ function stringy (number) {
   return string;
 }
 
+// test cases
 console.log(stringy(6));  // "101010"
 console.log(stringy(9));  // "101010101"
 console.log(stringy(4));  // "1010"
