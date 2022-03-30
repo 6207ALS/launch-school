636546553a8a994425ca4f9016eb7d83a0ad0073
Allen Lee <6207ALS@gmail.com>
2022-03-29 20:29:22 -0400

    Easy 4: Palindrome Strings (Part 1)

---
+++ easy-4/palindrome.js
@@ -0,0 +1,19 @@
+/*
+The isPalindrome function determines if if the string passed as an argument is 
+a palindrome. A palindrome reads the same forwards and backwards. For this case,
+the case matters and all, including non-alphabetical, characters matter.
+
+The function simply compares the string to its reversed version. You can 
+reverse a string by placing its characters in an array, reversing order of the
+elements, and rejoining the elements back together.  
+*/
+
+function isPalindrome (string) {
+    return string === string.split('').reverse().join('');
+}
+
+// test cases
+console.log(isPalindrome('madam'));               // true
+console.log(isPalindrome('Madam'));               // false (case matters)
+console.log(isPalindrome("madam i'm adam"));      // false (all characters matter)
+console.log(isPalindrome('356653'));              // true
