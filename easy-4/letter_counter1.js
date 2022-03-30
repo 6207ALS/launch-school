40cd450e95f1ac1d8ee581ad7491223e6e5dd263
Allen Lee <6207ALS@gmail.com>
2022-03-29 22:41:35 -0400

    Easy 4: Letter Counter (Part 1) (Edit)

--- easy-4/letter_counter1.js
+++ easy-4/letter_counter1.js
@@ -29,5 +29,5 @@ function wordSizes (string) {
 console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
 console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 1, "7": 2 }
 console.log(wordSizes("What's up doc?"));                              // { "2": 1, "4": 1, "6": 1 }
+console.log(wordSizes('    hello'));                                   // { '5': 1 }
 console.log(wordSizes(''));                                            // {}
-console.log(wordSizes('    hello'));    
