aec762852064d28e93876bfb0b20150cf45b25d4
Allen Lee <6207ALS@gmail.com>
2022-04-05 16:33:10 -0400

    String and Text Processing: Search Word (Part 2)

---
+++ string-text-processing/search_word2.js
@@ -0,0 +1,26 @@
+/*
+In continuation of exercise "String and Text Processing: Search Word 
+(Part 2)"...
+The searchWord function takes a word and some text as arguments, and returns the
+text with every instance of the word highlighted. A highlighted word is enclosed 
+with two asterisks ('**') on each side and every letter capitalized 
+(e.g., '**HIGHLIGHTEDWORD**').
+
+The function constructs a new Regex object to define a pattern. The pattern 
+strictly matches the given word, excluding words that contain the word argument.
+Words that match the pattern are replaced with the highlighted format of the 
+word.  
+*/
+
+function searchWord (word, text) {
+  let regex = new RegExp("(^|\\s)" + word + "($|\\s)", 'ig')
+  text = text.replace(regex, `**${word.toUpperCase()}**`);
+  return text;
+}
+
+// test case
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?';
+
+console.log(searchWord('sed', text));
+// returns
+// "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?"
