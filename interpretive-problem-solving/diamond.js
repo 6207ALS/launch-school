46b55a393ec6a7f576d0bcf428a4da77c2ed95ff
Allen Lee <6207ALS@gmail.com>
2022-04-13 20:01:03 -0400

    Interpretive Problem Solving: Diamonds (Edit)

--- interpretive-problem-solving/diamond.js
+++ interpretive-problem-solving/diamond.js
@@ -3,26 +3,28 @@ The diamond function displays a four-pointed diamond in an nxn grid, where n is
 an odd integer supplied as an argument to the function. It is assumed that the 
 argument will always be an odd integer.
 
-The function breaks down the process into two parts: the top half of the diamond
-and the bottom half of the diamond. Every line is composed of whitespace 
-characters and asterisk(s). A formula is used to determine the number of 
-whitespace characters: (n - (# of asterisks needed for the line)) / 2. 
-The number of asterisks needed for every line (for the top half) is the series 
-of odd numbers all the way to n. The process for the bottom half is similar. 
-The bottom half begins n - 2 number of asterisks and decrements by 2 until it
-reaches to 1 asterisk. 
+The function first determines the number of asterisks needed for each line of 
+the diamond. From [1, n], every odd number is recorded in an array. And from 
+(n, 1], every odd number is recorded again. In addition to number of asterisks 
+per line, the function must also determine the number of whitespace characters 
+per line. A general formula is used: (n - (# of asterisks)) / 2. Iterating over
+each number in the list, a number of whitespace characters and asterisks is 
+printed. 
 */
 
 function diamond (n) {
-  for (let i = 1; i <= n; i += 2) {
-    let space = (n - i) / 2;
-    console.log(' '.repeat(space) + '*'.repeat(i));
+  let asteriskSequence = [];
+  let increment = 2; 
+  let num = 1;
+  while (num > 0) {
+    asteriskSequence.push(num);
+    if (num === n) increment = -2
+    num += increment;
   }
-
-  for (let j = n - 2; j > 0; j -= 2) {
-    let space = (n - j) / 2;
-    console.log(' '.repeat(space) + '*'.repeat(j));
-  }
+  
+  asteriskSequence.forEach(asterisk => {
+    console.log(' '.repeat((n - asterisk) / 2) + '*'.repeat(asterisk));
+  });
 }
 
 diamond(1);
