378b358c854c50d6c357177e7fa081c5355f6596
Allen Lee <6207ALS@gmail.com>
2022-04-14 20:08:09 -0400

    Debugging JS: Task List

---
+++ debugging-js/task_list.js
@@ -0,0 +1,55 @@
+/*
+The completeTasks function had a logical error when deleting a completed task.
+It uses the "delete" operator in attempt to remove the first task on the list. 
+The "delete" operator, however, is used to remove a property from an object.
+When used on an array element, the length of the array remains the same, with 
+an empty item at the index. One solution would be to use the Array.prototype
+.shift() method to remove the first element of an array, updating the length of
+the new array.
+*/
+
+let todos = ['wash car', 'exercise', 'buy groceries', 'balance budget',
+             'call plumber', 'feed fido', 'get gas',  'organize closet'];
+
+function addTask(task) {
+  if (todos.includes(task)) {
+    console.log('That task is already on the list.');
+  } else {
+    todos.push(task);
+  }
+}
+
+function completeTasks(n = 1) {
+  let tasksComplete = 0;
+
+  while (todos.length > 0 && tasksComplete < n) {
+    console.log(`${todos[0]} complete!`);
+    todos.shift();
+    tasksComplete++;
+  }
+
+  if (todos.length === 0) {
+    console.log('All tasks complete!');
+  } else {
+    console.log(`${tasksComplete} tasks completed; ${todos.length} remaining.`);
+  }
+}
+
+function displayTaskList() {
+
+  console.log(`ToDo list (${todos.length} tasks):`);
+  console.log('---------------------');
+
+  for (let idx = 0; idx < todos.length; idx++) {
+    console.log(`-- ${todos[idx]}`);
+  }
+}
+
+// Utilizing our task manager
+
+addTask('oil change');
+addTask('dentist');
+addTask('homework');
+
+completeTasks(3);
+displayTaskList();
