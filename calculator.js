71c8425b8443cc7e6406c0d80e237c882fa2583a
Allen Lee <6207ALS@gmail.com>
2022-04-18 15:32:36 -0400

    Lesson 2: Walk-through: Calculator (Edit)

--- calculator.js
+++ calculator.js
@@ -8,16 +8,19 @@ prompting if provided an invalid input.
 let rlSync = require('readline-sync');
 console.log('Welcome to Calculator!');
 
+// prompt user for first number
 let num1;
 do {
   num1 = Number(rlSync.question("What's the first number?\n> ").trim());
 } while (!Number(num1));
 
+// prompt user for second number
 let num2;
 do {
   num2 = Number(rlSync.question("What's the second number?\n> ").trim() );
 } while (!Number(num2));
 
+// prompt user for operation from a given list
 let userOp;
 let operations = ["add", "subtract", "multiply", "divide", '1', '2', '3', '4'];
 
@@ -26,6 +29,7 @@ do {
 1) Add \n2) Subtract \n3) Multiply \n4) Divide\n> ").toLowerCase().trim();
 } while (!operations.includes(userOp));
 
+// perform given operation on two numbers
 let result;
 switch (userOp.toLowerCase()) {
   case '1':
@@ -45,4 +49,6 @@ switch (userOp.toLowerCase()) {
     result = num1 / num2; 
     break;
 }
+
+// log result to console
 console.log(`The result is: ${result}`);
